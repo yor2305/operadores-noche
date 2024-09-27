7041aefc1019a88c250516fe9532fd3afee35155
Yordy Jefferson Fajardo Castillo <yordijefferson23@gmail.com>
2024-09-26 20:01:58 -0500

    se crea el archivo OperadoresDeAsignacion

---
+++ operadoresProject/OperadoresDeAsignacion.py
@@ -0,0 +1,19 @@
+#Ejemplo con operadores de Asignacion
+
+var1 = 5
+var2 = 12
+var3 = 8
+var4 = 14
+var5 = 6
+var6 = 8
+
+
+var1 += 3 # var1 = var1+3 asignacion con suma
+var2 -= 3 # lo mismo aplica para los demas operadores de asignacion
+var3 *= 3
+var4 /= 3
+var5 %= 3
+var6 //= 3
+print(var1,var2,var3,var4,var5,var6)
+
+
